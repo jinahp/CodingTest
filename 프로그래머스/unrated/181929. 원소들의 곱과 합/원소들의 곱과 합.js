d8eb9a0d81ebcee65689416753591ca34223ba15
Jinah Park <113954463+jinahp@users.noreply.github.com>
2023-11-01 14:31:08 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let sum = 0;
+    let multiply = 1;
+    for (let i = 0; i < num_list.length; i++) {
+        sum += num_list[i];
+        multiply *= num_list[i];
+    
+    }
+    return multiply < Math.pow(sum, 2) ? 1 : 0;
+}
