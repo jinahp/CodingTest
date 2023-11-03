84e32af0efc84a2deee4f3669251930663bd5dfd
Jinah Park <113954463+jinahp@users.noreply.github.com>
2023-11-03 13:17:19 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    let answer;
+    
+    if (a % 2 === 1 && b % 2 === 1) {
+        answer = Math.pow(a, 2) + Math.pow(b, 2);
+    } else if (a % 2 === 1 || b % 2 === 1) {
+        answer = 2 * (a + b);
+    } else {
+        answer = Math.abs(a - b);
+    }
+    
+    return answer;
+}
